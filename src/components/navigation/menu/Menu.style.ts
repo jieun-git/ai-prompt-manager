368a35e49edcd77540936e8b0c090f6bc8d097ec
jieun-git <forworkje21@gmail.com>
2025-03-27 15:20:54 +0900

    Styles: Menu 컴포넌트 퍼블리싱

--- src/components/navigation/menu/Menu.style.ts
+++ src/components/navigation/menu/Menu.style.ts
@@ -4,6 +4,18 @@ import styled from 'styled-components'
 const S_Menu = styled(Menu)`
     height: 100%;
     background-color: var(--background-z-default);
+
+    .ant-menu-item {
+        color: var(--blue-z-02);
+        
+        &.ant-menu-item-selected {
+            color: var(--blue-z-04);
+            border-radius: var(--radius-01);
+        }
+        &:not(.ant-menu-item-selected):not(.ant-menu-submenu-selected):hover {
+            color: var(--yellow-z-01);
+        }
+    }
     
 `
 
