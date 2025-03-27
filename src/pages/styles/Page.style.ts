43682332613e400ac519a3b79d9193d75ee0ef01
jieun-git <forworkje21@gmail.com>
2025-03-27 21:08:15 +0900

    Feat: S_DisabledContent 스타일 추가

--- src/pages/styles/Page.style.ts
+++ src/pages/styles/Page.style.ts
@@ -1,17 +1,34 @@
 import styled from 'styled-components'
 
-export interface Flex {
+export interface FlexProps {
     flexDirection?: 'column' | 'row';
     gap?: number
     alignItems?: 'flex-start' | 'center' | 'flex-end'
-    justifyContent?: 'flex-start' | 'center' | 'flex-end'
+    justifyContent?: 'flex-start' | 'center' | 'flex-end' | 'space-between'
+    padding?: string
 }
 
-export const S_FlexWrapper = styled.div<Flex>`
+export interface DisabledContentProps {
+    disabled?: boolean
+}
+
+export const S_FlexWrapper = styled.div<FlexProps>`
     display: flex;
     flex-direction: ${({ flexDirection }) => flexDirection};
     gap: ${({ gap }) => gap}px;
     align-items: ${({ alignItems }) => alignItems};
     justify-content: ${({ justifyContent }) => justifyContent};
+    padding: ${({ padding }) => padding};
+
 `
 S_FlexWrapper.displayName = 'S_FlexWrapper'
+
+export const S_DisabledContent = styled.div<DisabledContentProps>`
+    ${({ disabled }) => disabled ? ` pointer-events: none;
+    background: rgba(211, 211, 211, 0.8);
+    border-radius: var(--radius-02);
+    opacity: 0.5;` : null}
+   
+
+`
+S_DisabledContent.displayName = 'S_DisabledContent'
