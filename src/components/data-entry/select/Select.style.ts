6ba40bd79cbfa53c4e7fcadea14ee64c3ab55dd5
jieun-git <forworkje21@gmail.com>
2025-05-21 16:13:05 +0900

    Styles : Select disabled 스타일 추가

--- src/components/data-entry/select/Select.style.ts
+++ src/components/data-entry/select/Select.style.ts
@@ -1,9 +1,13 @@
 import { Select } from 'antd'
 import styled from 'styled-components'
-import {ExtendedSelectProps} from "./Select";
+import { ExtendedSelectProps } from './Select'
 
 export const S_Select = styled(Select)<ExtendedSelectProps>`
     width: ${({ width }) => width} !important;
+
+    &.ant-select-disabled .ant-select-selector {
+        background-color: var(--white) !important;
+        color: var(--blue-z-04) !important;
+    }
 `
 S_Select.displayName = 'S_Select'
-
