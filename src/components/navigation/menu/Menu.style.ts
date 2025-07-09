6796588e790bb537553085d50ef1d4a53d4979f4
jieun-git <forworkje21@gmail.com>
2025-07-10 08:36:51 +0900

    Styles: 메뉴 컴포넌트 서브 메뉴 관련 스타일 적용

--- src/components/navigation/menu/Menu.style.ts
+++ src/components/navigation/menu/Menu.style.ts
@@ -1,22 +1,36 @@
-import {Menu} from 'antd'
+import { Menu } from 'antd'
 import styled from 'styled-components'
 
 const S_Menu = styled(Menu)`
     height: 100%;
     background-color: var(--background-z-default);
 
-    .ant-menu-item {
+    .ant-menu-submenu-title {
         color: var(--blue-z-02);
-        
-        &.ant-menu-item-selected {
-            color: var(--blue-z-04);
-            border-radius: var(--radius-01);
-        }
-        &:not(.ant-menu-item-selected):not(.ant-menu-submenu-selected):hover {
+    }
+
+    .ant-menu-submenu-selected > .ant-menu-submenu-title {
+        color: var(--blue-z-02);
+        &:hover {
             color: var(--yellow-z-01);
         }
     }
-    
+
+    .ant-menu-item {
+        color: var(--blue-z-02);
+        &:hover {
+            color: var(--yellow-z-01) !important;
+        }
+
+        &.ant-menu-item-selected {
+            color: var(--blue-z-04) !important;
+            font-weight: 800;
+
+            &:hover {
+                color: var(--yellow-z-01) !important;
+            }
+        }
+    }
 `
 
 S_Menu.displayName = 'S_Menu'
