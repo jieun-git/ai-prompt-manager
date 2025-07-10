a0de2fb10efa119cc08953260c117a35e398ff5c
jieun-git <forworkje21@gmail.com>
2025-07-10 14:01:22 +0900

    Styles: Select 컴포넌트 스타일 수정(hover, background, color 수정)

--- src/components/data-entry/select/Select.style.ts
+++ src/components/data-entry/select/Select.style.ts
@@ -5,9 +5,31 @@ import { ExtendedSelectProps } from './Select'
 export const S_Select = styled(Select)<ExtendedSelectProps>`
     width: ${({ width }) => width} !important;
 
-    &.ant-select-disabled .ant-select-selector {
-        background-color: var(--white) !important;
-        color: var(--blue-z-04) !important;
+    && .ant-select-selector {
+        border: 1px solid var(--green-z-01);
+        background: var(--background);
+        color: var(--white);
+
+        &:hover {
+            border-color: var(--yellow-z-01);
+        }
+    }
+
+    && .ant-select-selection-placeholder {
+        color: var(--text-placeholder);
+    }
+
+    &&.ant-select-disabled .ant-select-selector {
+        background-color: var(--disabled-input);
+        color: var(--text-placeholder);
+
+        &:hover {
+            border-color: var(--green-z-01);
+        }
+    }
+
+    && .ant-select-arrow {
+        color: var(--green-z-01);
     }
 `
 S_Select.displayName = 'S_Select'
