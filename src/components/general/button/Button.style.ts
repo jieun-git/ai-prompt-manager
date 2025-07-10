e9bbc4394c7f323c857e2df2f86cb0b5b9be08e7
jieun-git <forworkje21@gmail.com>
2025-07-10 15:12:55 +0900

    Styles: primary, text, default, link 타입에 따른 버튼 스타일 정의

--- src/components/general/button/Button.style.ts
+++ src/components/general/button/Button.style.ts
@@ -1,61 +1,72 @@
 import { Button } from 'antd'
 import styled from 'styled-components'
-import {Typo_label_01} from "../../../styles/constants/typogrphy";
-import {ExtendButtonProps} from "./Button";
+import { ExtendButtonProps } from './Button'
 
 const S_Button = styled(Button)<ExtendButtonProps>`
     border-radius: var(--radius-01);
-    background: ${({ type }) => type === 'primary' ? 'var(--blue-z-03)' : 'transparent' };
-    width: ${({ fixedSize }) => fixedSize ? '10rem' : 'auto'};
+    width: ${({ fixedSize }) => (fixedSize ? '10rem' : 'auto')};
 
-    // todo 타입별 스타일, 각 hover 스타일 정의
-    // background: var(--button-secondary);
-    // border: 0;
-    // border-radius: 0;
-    // color: var(--text-on-color);
-    // text-shadow: none;
-    // ${Typo_label_01};
-    // &.ant-btn-primary:not(.ant-btn-background-ghost) {
-    //     background: var(--button-primary);
-    //     &:hover {
-    //         background: var(--button-primary-hover);
-    //     }
-    //     &:active {
-    //         background: var(--button-primary-active);
-    //     }
-    // }
-     &.ant-btn-text {
-         background: transparent;
-         color: var(--blue-z-03);
-     }
-    
-    &.ant-btn-variant-text:not(:disabled):not(.ant-btn-disabled):hover {
-        color: var(--yellow-z-01);
+    // default
+    &.ant-btn {
+        background: transparent;
+        color: var(--blue-z-03);
+        border: 1px solid var(--blue-z-03);
+
+        &:hover {
+            border: 1px solid var(--yellow-z-01) !important;
+            color: var(--yellow-z-01) !important;
+            background: transparent !important;
+        }
+
+        &:disabled,
+        &.ant-btn-disabled {
+            color: var(--text-disabled);
+            opacity: 0.5;
+            cursor: not-allowed;
+
+            &:hover {
+                color: var(--text-disabled) !important;
+                text-decoration: none;
+            }
+        }
     }
-    
-    &.ant-btn-variant-solid:not(:disabled):not(.ant-btn-disabled):hover {
-        background: var(--yellow-z-01);
+
+    // primary
+    &.ant-btn-primary {
+        background: var(--blue-z-03);
+        color: var(--white);
+        border: none;
+
+        &:hover {
+            background: var(--yellow-z-01) !important;
+            color: var(--white) !important;
+        }
     }
-    // &.ant-btn-link {
-    //     background: transparent;
-    //     color: var(--link-primary);
-    // }
-    // &.ant-btn-background-ghost:not(.ant-btn-primary) {
-    //     color: var(--icon-secondary);
-    // }
-    // &.ant-btn.ant-btn-background-ghost:not(.ant-btn-primary)[disabled] {
-    //     color: var(--icon-on-color-disabled);
-    // }
-    // &.ant-btn-background-ghost.ant-btn-primary {
-    //     color: var(--link-primary);
-    //     border-color: var(--button-primary);
-    //     &:hover {
-    //         border-color: var(--button-primary-hover);
-    //     }
-    //     &:active {
-    //         border-color: var(--button-primary-active);
-    //     }
-    // }
+
+    // text
+    &.ant-btn-text {
+        background: transparent;
+        color: var(--blue-z-03);
+        border: none;
+
+        &:hover {
+            color: var(--yellow-z-01);
+            border: none !important;
+        }
+    }
+
+    // link
+    &.ant-btn-link {
+        border: none;
+        background: transparent;
+        color: var(--blue-z-02);
+
+        &:hover {
+            color: var(--yellow-z-01);
+            border: none !important;
+            text-decoration: underline;
+        }
+    }
 `
 S_Button.displayName = 'S_Button'
 
