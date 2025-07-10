7a8701e630a7a5cc1571ebd290940431cc17fb2d
jieun-git <forworkje21@gmail.com>
2025-07-10 14:26:52 +0900

    Cleanup: 사용하지 않는 스타일 제거

--- src/components/data-entry/form/Form.style.ts
+++ src/components/data-entry/form/Form.style.ts
@@ -1,24 +1,23 @@
-import {Form} from 'antd'
+import { Form } from 'antd'
 import styled from 'styled-components'
-import {Typo_heading_04_bold, Typo_label_02} from "../../../styles/constants/typogrphy";
-import {ExtendedFormItemProps} from "./Form";
+import {
+    Typo_heading_04_bold,
+    Typo_label_02,
+} from '../../../styles/constants/typogrphy'
+import { ExtendedFormItemProps } from './Form'
 
-export const S_Form = styled(Form)`
-    border: 2px solid var(--green-z-01);
-    border-radius: var(--radius-02);
-    padding: var(--spacing-04) var(--spacing-06);
-`
+export const S_Form = styled(Form)``
 S_Form.displayName = 'S_Form'
 
 export const S_FormItem = styled(Form.Item)<ExtendedFormItemProps>`
-        width: ${({ width }) => width} !important;
+    width: ${({ width }) => width} !important;
 
-        .ant-form-item-label > label {
+    .ant-form-item-label > label {
         color: var(--blue-z-02);
         ${Typo_label_02}
 
         &.ant-form-item-required::before {
-                color: var(--yellow-z-01) !important;
+            color: var(--yellow-z-01) !important;
         }
     }
 `
@@ -27,6 +26,5 @@ S_FormItem.displayName = 'S_FormItem'
 export const S_FormTitle = styled.h1`
     ${Typo_heading_04_bold};
     color: var(--green-z-01);
-
 `
 S_FormTitle.displayName = 'S_FormTitle'
