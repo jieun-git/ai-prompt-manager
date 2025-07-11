c5e086dcf01bae65f9a5438ab87b2f1a69df37a7
jieun-git <forworkje21@gmail.com>
2025-07-11 16:58:11 +0900

    Refactor: S_Description -> S_Helper 이름 변경

--- src/pages/prompt/components/form/CreatePromptForm.tsx
+++ src/pages/prompt/components/form/CreatePromptForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Form, Select, TextArea, Button } from '../../../../components'
-import { S_Description, S_FlexWrapper } from '../../../styles/Page.style'
+import { S_Helper, S_FlexWrapper } from '../../../styles/Page.style'
 import { PlusCircleOutlined } from '@ant-design/icons'
 
 const CreatePromptForm = () => {
@@ -10,14 +10,14 @@ const CreatePromptForm = () => {
                 <Select placeholder="Select a node name" />
             </Form.Item>
             <Form.Item label="Prompt" name="prompt" required>
-                <S_Description>
+                <S_Helper>
                     Define your prompt template. You can use{' '}
                     {String.raw`{{variable}}`} to insert variables into your
                     prompt. <br />
                     Note: Variables must be alphabetical characters or
                     underscores. You can also link other text prompts using the
                     plus button.
-                </S_Description>
+                </S_Helper>
                 <S_FlexWrapper
                     flexDirection="column"
                     margin="8px 0 0 0"
@@ -31,10 +31,10 @@ const CreatePromptForm = () => {
                 </S_FlexWrapper>
             </Form.Item>
             <Form.Item label="Commit message (Optional)" name="commitMessage">
-                <S_Description>
+                <S_Helper>
                     Provide information about the changed made in this version.
                     Helps maintain a clear history of prompt iterations.
-                </S_Description>
+                </S_Helper>
                 <TextArea
                     height={80}
                     resize={false}
