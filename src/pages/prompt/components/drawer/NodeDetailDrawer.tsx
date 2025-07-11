3b40d44b26973314c29e733765f1134149d4f6df
jieun-git <forworkje21@gmail.com>
2025-07-11 16:58:46 +0900

    Wip:  에러 처리 전 커밋

--- src/pages/prompt/components/drawer/NodeDetailDrawer.tsx
+++ src/pages/prompt/components/drawer/NodeDetailDrawer.tsx
@@ -1,11 +1,9 @@
 import { Drawer } from '../../../../components'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NodeData } from '../table/PromptManageTable'
-import {
-    S_FlexWrapper,
-    S_PromptDetailContent,
-    S_PromptListContent,
-} from '../../../styles/Page.style'
+import { S_FlexWrapper } from '../../../styles/Page.style'
+import PromptVersionDetail from '../../PromptVersionDetail'
+import PromptVersionList from '../../PromptVersionList'
 
 interface NodeDetailDrawerProps {
     openDrawer: boolean
@@ -13,18 +11,63 @@ interface NodeDetailDrawerProps {
     onClose: () => void
 }
 
+const versionList = [
+    {
+        version: '1',
+        name: '임시1',
+        date: '2025.6.14. 오전 11:45:35',
+        isProduction: false,
+        prompt: {
+            system: 'AI 전문가1 System',
+            user: 'AI 전문가1 User',
+            assistant: 'AI 전문가1 Assistant',
+        },
+    },
+    {
+        version: '2',
+        name: '임시2',
+        date: '2025.6.15. 오전 11:45:35',
+        isProduction: true,
+        prompt: {
+            system: 'AI 전문가2 System',
+            user: 'AI 전문가2 User',
+            assistant: 'AI 전문가2 Assistant',
+        },
+    },
+    {
+        version: '3',
+        name: '임시3',
+        date: '2025.6.16. 오전 11:45:35',
+        isProduction: false,
+        prompt: {
+            system: 'AI 전문가3 System',
+            user: 'AI 전문가3 User',
+            assistant: 'AI 전문가3 Assistant',
+        },
+    },
+]
+
 const NodeDetailDrawer = ({
     openDrawer,
     selectedRow,
     onClose,
 }: NodeDetailDrawerProps) => {
-    const PromptListContent = () => {
-        return <S_PromptListContent>List</S_PromptListContent>
+    const [versionDetail, setVersionDetail] = useState<any>(undefined)
+    const [defaultProduction, setDefaultProduction] = useState<any>(undefined)
+
+    const handleSetVersionDetail = (data: any) => {
+        setVersionDetail(data)
     }
 
-    const PromptDetailContent = () => {
-        return <S_PromptDetailContent>Detail</S_PromptDetailContent>
+    const handleChangeDefaultProduction = (version: string) => {
+        setDefaultProduction(version)
     }
+
+    useEffect(() => {
+        setVersionDetail(versionList[0])
+        const productionVersion = versionList.find((item) => item.isProduction)
+        setDefaultProduction(productionVersion?.version)
+    }, [openDrawer])
 
     return (
         <Drawer
@@ -33,9 +76,17 @@ const NodeDetailDrawer = ({
             size="large"
             onClose={onClose}
         >
-            <S_FlexWrapper flexDirection="row" height="100%" padding="0 24px">
-                {PromptListContent()}
-                {PromptDetailContent()}
+            <S_FlexWrapper flexDirection="row" height="100%">
+                <PromptVersionList
+                    defaultProduction={defaultProduction}
+                    versionList={versionList}
+                    onSetVersionDetail={handleSetVersionDetail}
+                />
+                <PromptVersionDetail
+                    defaultProduction={defaultProduction}
+                    versionDetail={versionDetail}
+                    onChangeDefaultProduction={handleChangeDefaultProduction}
+                />
             </S_FlexWrapper>
         </Drawer>
     )
