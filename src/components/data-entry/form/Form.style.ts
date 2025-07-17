0b715aefffababa9c8f0906f42282b97335a7de2
jieun <jeahn@nkia.co.kr>
2025-07-17 10:22:47 +0900

    Feat:  FormList 스타일 추가

--- src/components/data-entry/form/Form.style.ts
+++ src/components/data-entry/form/Form.style.ts
@@ -23,6 +23,20 @@ export const S_FormItem = styled(Form.Item)<ExtendedFormItemProps>`
 `
 S_FormItem.displayName = 'S_FormItem'
 
+export const S_FormList = styled(Form.List)<ExtendedFormItemProps>`
+    width: ${({ width }) => width} !important;
+
+    .ant-form-item-label > label {
+        color: var(--blue-z-02);
+        ${Typo_label_02}
+
+        &.ant-form-item-required::before {
+            color: var(--yellow-z-01) !important;
+        }
+    }
+`
+S_FormList.displayName = 'S_FormList'
+
 export const S_FormTitle = styled.h1`
     ${Typo_heading_04_bold};
     color: var(--green-z-01);
--- src/components/data-entry/form/Form.tsx
+++ src/components/data-entry/form/Form.tsx
@@ -1,6 +1,6 @@
-import {S_Form, S_FormItem} from "./Form.style";
+import { S_Form, S_FormItem } from './Form.style'
 import React, { PropsWithChildren } from 'react'
-import {FormProps, FormItemProps} from "antd";
+import { FormProps, FormItemProps, Form as AntForm } from 'antd'
 
 export interface ExtendedFormItemProps extends FormItemProps {
     width?: string
@@ -14,5 +14,10 @@ const FormItem = ({ children, ...props }: ExtendedFormItemProps) => {
     return <S_FormItem {...props}>{children}</S_FormItem>
 }
 
+const FormList = AntForm.List
+
+Form.List = FormList
 Form.Item = FormItem
+Form.useForm = AntForm.useForm
+
 export default Form
